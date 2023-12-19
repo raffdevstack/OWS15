f1afeefdd338d8628a43b01b20185c1e4b6274bd
pseudocat <sydneyang1234@gmail.com>
2023-12-19 21:27:54 +0800

    added scholarship.blade.php

---
+++ resources/views/student/scholarship.blade.php
@@ -0,0 +1,112 @@
+
+<div style="margin-left: 5%; margin-right: 5%;">
+    <div class="mt-6 rounded">
+        <div class="bg--100 relative flex justify-start border-box truncate w-auto mb-2 p-2 bg-white border border-gray-300 rounded-lg hover:shadow-lg shadow-lg">
+            <div class="w-full bg--100 md:items-left flex flex-col truncate">
+                <div class="h-full items-center flex">
+                    <a href="#">
+                        <h4 class="text-md text-gray-700 truncate">My Scholarship</h4>
+                    </a>
+                </div>
+                <!-- status -->
+                <div class="bg-300 mt-2 mb-4 pl-3  lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 gap-2">
+                  <div class="bg-300 border text-gray-600 rounded-lg p-2 hover:shadow-md transition-all">
+                      <div class="flex items-center gap-1">
+                          <h4 class="truncate font-medium">Ched Scholarship Program Grantee</h4>
+                      </div>  
+                      <div class="flex mt-2 font-medium justify-end gap-1">
+                          <button>
+                          <a href="#" class="bg-red-900 text-white text-sm p-2 flex items-center rounded-full hover:bg-red-700 transition-all">
+                              <span class="px-4 rounded-full">Status</span>
+                          </a>
+                      </button>
+                      </div>
+                  </div>
+              </div>
+              
+                    <!--  -->
+            </div>
+        </div>
+        <div class="mt-2 mb-4 gap-2 flex justify-end">
+          <span class="flex gap-1">
+              <select id="#" style="border: 1px solid black; border-radius: 30px;" class="bg-white rounded-lg  border-gray-950 text-black text-sm  focus:ring-black focus:border-black block w-full p-1 md:p-2 dark:bg-black dark:border-black dark:placeholder-black dark:text-black dark:focus:ring-black dark:focus:border-black">
+                  <option selected>All</option>
+                  <option value="latest">Latest</option>
+                  <option value="oldest">Oldest</option>
+              </select>
+          </span>
+      </div>
+      
+<!-- scholarship -->
+<div class="mb-4  ">
+<div class="bg--100 relative flex justify-start border-box truncate w-full mb-2 p-2 bg-gray-200 border border-gray-300 rounded-lg hover:shadow-lg shadow-sm">
+<div class="bg-300 mt-2 mb-4 lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 gap-2 p-2">
+<img class="rounded-xl" src="img/ows logo (1).png" alt="">  
+<div class="p-3" >
+<ul>
+  <li> <h2>hey</h2></li>
+  <li><h3>wer</h3></li>
+  <li>
+      <h3 class="text-justify">fgfjkasfkjsfajsgfhfgasgsjfnmf
+          
+      </h3>
+  </li>
+</ul>
+</div>
+<div class="flex  font-medium justify-end gap-1 p-8 " style="margin-left: 10px; margin-top: 60px;">
+<button>
+<a href="#" class="bg-red-900 text-white text-sm p-2 flex items-center rounded-full hover:bg-red-700 transition-all">
+  <span class="px-1 rounded-full">MORE DETAILS</span>
+</a>
+</button>
+</div>
+
+</div>
+</div>
+<div class="bg--100 relative flex justify-start border-box truncate w-full mb-2 p-2 bg-gray-200  border border-gray-300 rounded-lg hover:shadow-lg shadow-sm">
+<div class="bg-300 mt-2 mb-4 lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 gap-2 p-2">
+  <img class="rounded-xl" src="img/ows logo (1).png" alt="">  
+ <div class="p-3">
+  <ul>
+      <li> <h2>hey</h2></li>
+      <li><h3>wer</h3></li>
+      <li>
+          <h3 class="text-justify">fgfjkasfkjsfajsgfhfgasgsjfnmf
+              
+          </h3>
+      </li>   
+  </ul>
+ </div>
+ <div class="flex  font-medium justify-end gap-1 p-8 " style="margin-left: 10px; margin-top: 60px;">
+  <button>
+  <a href="#" class="bg-red-900 text-white text-sm p-2 flex items-center rounded-full hover:bg-red-700 transition-all">
+      <span class="px-1 rounded-full">MORE DETAILS</span>
+  </a>
+</button>
+</div>
+</div>
+</div>
+<div class="bg--100 relative flex justify-start border-box truncate w-full mb-2 p-2 bg-gray-200 border border-gray-300 rounded-lg hover:shadow-lg shadow-sm">
+<div class="bg-300 mt-2 mb-4 lg:grid lg:grid-cols-3 md:grid md:grid-cols-2 gap-2 p-2">
+  <img class="rounded-xl" src="img/ows logo (1).png" alt="">  
+ <div class="p-3">
+  <ul>
+      <li> <h2>Who may Apply?</h2></li>
+      <li>
+          <ul class="">
+              <h3 class="text-justify">fgfjkasfkjsfajsgfhfgasgsjfnmf
+              </h3>
+          </ul>
+      </li>
+  </ul>
+ </div>
+ <div class="flex  font-medium justify-end gap-1 p-8 " style="margin-left: 10px; margin-top: 60px;">
+  <button>
+  <a href="#" class="bg-red-900 text-white text-sm p-2 flex items-center rounded-full hover:bg-red-700 transition-all">
+      <span class="px-1 rounded-full">MORE DETAILS</span>
+  </a>
+</button>
+</div>  
+</div>
+</div>
+</div>
